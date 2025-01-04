8adef66436157dddc8fbe8e1a9591b5328d967c2
Pandxr4 <mnico.amaya38@gmail.com>
2025-01-04 17:19:01 -0300

    v2.0

--- js/pruebas.js
+++ js/pruebas.js
@@ -16,34 +16,14 @@ function convertirARS(cantidad, tipoDeCambio) {
   }
 }
 
-/**
- * Función para convertir USD a ARS según el tipo de cambio.
- * @param {number} cantidad - La cantidad de USD a convertir.
- * @param {string} tipoDeCambio - El tipo de cambio a utilizar.
- * @returns {string|number} - El resultado de la conversión o un mensaje de error.
- */
-function convertirUSD(cantidad, tipoDeCambio) {
-  const cambio = tiposDeCambio.find(cambio => cambio.tipo === tipoDeCambio);
-  if (cambio) {
-    const resultado = cantidad * cambio.valor;
-    return resultado.toFixed(2);
-  } else {
-    return 'Tipo de cambio no válido';
-  }
-}
-
 // Función para iniciar el conversor
 function iniciarConversor() {
   const nombreUsuarioInput = document.getElementById('nombreUsuario');
   const confirmarNombreBtn = document.getElementById('confirmarNombre');
   const tipoDeCambioSelect = document.getElementById('tipoDeCambio');
   const cantidadARSInput = document.getElementById('cantidadARS');
-  const convertirARSBtn = document.getElementById('convertirARS');
-  const resultadoARSP = document.getElementById('resultadoARS');
-  const tipoDeCambioInversoSelect = document.getElementById('tipoDeCambioInverso');
-  const cantidadUSDInput = document.getElementById('cantidadUSD');
-  const convertirUSDBtn = document.getElementById('convertirUSD');
-  const resultadoUSDP = document.getElementById('resultadoUSD');
+  const convertirBtn = document.getElementById('convertir');
+  const resultadoP = document.getElementById('resultado');
 
   let nombreUsuario = '';
 
@@ -56,18 +36,11 @@ function iniciarConversor() {
     }
   });
 
-  convertirARSBtn.addEventListener('click', () => {
+  convertirBtn.addEventListener('click', () => {
     const tipoDeCambio = tipoDeCambioSelect.value;
     const cantidadARS = parseFloat(cantidadARSInput.value);
     const resultado = convertirARS(cantidadARS, tipoDeCambio);
-    resultadoARSP.innerText = `${cantidadARS} ARS son equivalentes a ${resultado} USD según el tipo de cambio ${tipoDeCambio} al día de hoy.`;
-  });
-
-  convertirUSDBtn.addEventListener('click', () => {
-    const tipoDeCambio = tipoDeCambioInversoSelect.value;
-    const cantidadUSD = parseFloat(cantidadUSDInput.value);
-    const resultado = convertirUSD(cantidadUSD, tipoDeCambio);
-    resultadoUSDP.innerText = `${cantidadUSD} USD son equivalentes a ${resultado} ARS según el tipo de cambio ${tipoDeCambio} al día de hoy.`;
+    resultadoP.innerText = `${cantidadARS} ARS son equivalentes a ${resultado} USD según el tipo de cambio al día de hoy(${tipoDeCambio}).`;
   });
 }
 
