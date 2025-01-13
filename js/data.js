809054df43b55c05089cd8c46c163e1c30b16eab
Pandxr4 <mnico.amaya38@gmail.com>
2025-01-13 20:28:18 -0300

    git add .

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
 export const tiposDeCambio = [
-    { tipo: 'oficial', valor: 1059 },
-    { tipo: 'blue', valor: 1225 },
-    { tipo: 'mep', valor: 1164.01 },
-    { tipo: 'tarjeta', valor: 1376.70 },
-    { tipo: 'ccl', valor: 1184.07 },
+    { tipo: 'oficial', valor: 1059.50 },
+    { tipo: 'blue', valor: 1250 },
+    { tipo: 'mep', valor: 1163.80 },
+    { tipo: 'tarjeta', valor: 1377.35 },
+    { tipo: 'ccl', valor: 1184.15 },
   ];
   
